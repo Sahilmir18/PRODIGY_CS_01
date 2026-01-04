8b0428f7afb89c30fbddff35690676a7b12324d9
Sahilmir18 <140206773+Sahilmir18@users.noreply.github.com>
2026-01-04 17:48:56 +0530

    new code efficient

--- main.py
+++ main.py
@@ -1,76 +1,47 @@
+from tkinter import *
 import tkinter as tk
 
+window = tk.Tk()
+window.title("Caesar Cipher")
+window.geometry("400x400")
 
-def show_choice():
-    
-    selection = "You selected the option " + str(var.get())
-    label.config(text=selection)
+tk.Label(window, text = "Welcome to Caesar Cipher").pack(anchor='w')
+tk.Label(window, text = " ").pack(anchor='w')
+tk.Label(window, text = "Choose an option:").pack(anchor='w')
+opt = IntVar(value=1)
+tk.Radiobutton(window, text="Encrypt",variable=opt, value=1).pack(anchor='w')
+tk.Radiobutton(window, text="Decrypt",variable=opt, value=2).pack(anchor='w')
 
-root = tk.Tk()
-root.title("Caesar Cipher")
-root.geometry("400x250")
+tk.Label(window, text = "Cipher Shift:").pack(anchor='w')
+shift = tk.Entry(window, width=15)
+shift.pack(anchor='w')
 
-var = tk.IntVar()
+tk.Label(window, text = "Input text: ").pack(anchor='w')
+input = tk.Entry(window, width=40)
+input.pack(anchor='w')
+
+output_label = tk.Label(window, text = "")
+output_label.pack(anchor='w')
+
+def Caesar_cipher():
+    if opt.get() == 1:
+        encrypted_text = input.get()
+        shift_val = int(shift.get())
+        encrypted_text = ''.join(chr((ord(char) + shift_val - 32) % 95 + 32) for char in encrypted_text)
+        
+        output_label.config(text = "Encrypted Text: " + encrypted_text)
+        
+    elif opt.get() == 2:
+        decrypted_text = input.get()
+        shift_val = int(shift.get())
+        decrypted_text = ''.join(chr((ord(char) - shift_val - 32) % 95 + 32) for char in decrypted_text)
+        
+        output_label.config(text = "Decrypted Text: " + decrypted_text)
 
 
+   
 
-tk.Label(root, text="""Choose your options (Decrypy/Encrpyt):""",
-         justify=tk.LEFT, padx=20).pack(anchor=tk.W)
-
-
-R1 = tk.Radiobutton(root, text="Encrypt", variable=var, value=1, command=show_choice)
-R1.pack(anchor=tk.W) # anchor=tk.W aligns to the west (left)
-
-R2 = tk.Radiobutton(root, text="Decrypt", variable=var, value=2, command=show_choice)
-R2.pack(anchor=tk.W)
+tk.Button(window, text= "Submit", command= Caesar_cipher).pack(anchor='w')
 
 
-
-# A label to display the current selection
-label = tk.Label(root, text="None selected")
-label.pack(anchor=tk.W, padx=20, pady=10)
-
-
-def manipulate_text():
-    if var.get() == 1:
-        text = entry.get()
-        textupper = text.upper()
-        def encrypt(textupper):
-            translation_table = str.maketrans({
-                'A': 'D', 'B': 'E', 'C': 'F', 'D': 'G', 'E': 'H', 'F': 'I',
-                'G': 'J', 'H': 'K', 'I': 'L', 'J': 'M', 'K': 'N', 'L': 'O',
-                'M': 'P', 'N': 'Q', 'O': 'R', 'P': 'S', 'Q': 'T', 'R': 'U',
-                'S': 'V', 'T': 'W', 'U': 'X', 'V': 'Y', 'W': 'Z', 'X': 'A',
-                'Y': 'B', 'Z': 'C'
-            })
-            encrypted = textupper.translate(translation_table)
-            return encrypted
-        result = encrypt(textupper)
-        result_label.config(text="Encrypted Text: " + result)
-    elif var.get() == 2:    
-        text = entry.get()
-        textupper = text.upper()
-        def decrypt(textupper):
-            translation_table = str.maketrans({
-                'D': 'A', 'E': 'B', 'F': 'C', 'G': 'D', 'H': 'E', 'I': 'F',
-                'J': 'G', 'K': 'H', 'L': 'I', 'M': 'J', 'N': 'K', 'O': 'L',
-                'P': 'M', 'Q': 'N', 'R': 'O', 'S': 'P', 'T': 'Q', 'U': 'R',
-                'V': 'S', 'W': 'T', 'X': 'U', 'Y': 'V', 'Z': 'W', 'A': 'X',
-                'B': 'Y',  'C': 'Z'
-            })
-            decrypted = textupper.translate(translation_table)
-            return decrypted
-        result = decrypt(textupper)
-        result_label.config(text="Decrypted Text: " + result)
-
-    
-entry = tk.Entry(root, width=40)
-entry.pack(pady=10)
-
-button = tk.Button(root, text="Submit", command=manipulate_text)
-button.pack(pady=10)
-
-result_label = tk.Label(root, text="", font = ("Helvetica", 12))
-result_label.pack(pady=10)
-
-root.mainloop()
+window.mainloop()
